592b20de6cedf0d53c930b73038b9aff44e3be59
winniediana4 <deinawinnie84@gmail.com>
2021-05-17 10:37:15 +0300

    generate directive strikethrough

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
