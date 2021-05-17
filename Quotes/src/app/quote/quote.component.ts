6bdc530976591822c2d7a232b62f2554a98ad0a9
winniediana4 <deinawinnie84@gmail.com>
2021-05-17 11:15:38 +0300

    add completeDate

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -10,8 +10,8 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quotes:Quotes[] = [
-    new Quotes(1, 'To be or not to be? That is the question', 'William Shakespeare'),
-    new Quotes(2, 'A rose is a rose by any other name', 'William Shakespeare'),
+    new Quotes(1, 'To be or not to be? That is the question', 'William Shakespeare', new Date(2020,3,14)),
+    new Quotes(2, 'A rose is a rose by any other name', 'William Shakespeare', new Date(2020,3,14)),
   ];
 
   toggleDetails(index: number){
--- Quotes/src/app/quotes.ts
+++ Quotes/src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
-  showDescription: boolean;
-  constructor(public id: number, public name: string, public description: string) {
-    this.showDescription = false;
+  public showDescription: boolean;
+  constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    this.showDescription=false;
   }
 }
