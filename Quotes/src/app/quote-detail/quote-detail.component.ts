15af4c04b5591adf0175a80f2342d502180fa430
winniediana4 <deinawinnie84@gmail.com>
2021-05-17 08:54:04 +0300

    bind quote property from parent to QuoteDetail

--- Quotes/src/app/quote-detail/quote-detail.component.ts
+++ Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote-detail',
