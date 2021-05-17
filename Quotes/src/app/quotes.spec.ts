ad4ea63b183b8c9ff16b676107717a021955235f
winniediana4 <deinawinnie84@gmail.com>
2021-05-17 11:48:02 +0300

    add dateCounter

--- Quotes/src/app/quotes.spec.ts
+++ Quotes/src/app/quotes.spec.ts
@@ -2,6 +2,6 @@ import { Quotes } from './quotes';
 
 describe('Quotes', () => {
   it('should create an instance', () => {
-    expect(new Quotes()).toBeTruthy();
+    expect(new Quote()).toBeTruthy();
   });
 });
