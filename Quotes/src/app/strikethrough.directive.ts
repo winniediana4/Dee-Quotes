3c683d2de39b915fc5b2f23bd4697cf8825cb4d6
winniediana4 <deinawinnie84@gmail.com>
2021-05-17 10:59:50 +0300

    import HostListener

--- Quotes/src/app/strikethrough.directive.ts
+++ Quotes/src/app/strikethrough.directive.ts
@@ -1,12 +1,21 @@
-import { Directive,ElementRef} from '@angular/core';
+import { Directive,ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor(private elem:ElementRef){ }
-  
+    
+  constructor(private elem:ElementRef){}
+
+  @HostListener("click") onClicks(){
+    this.textDeco("line-through")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
   private textDeco(action:string){
     this.elem.nativeElement.style.textDecoration=action;
 
